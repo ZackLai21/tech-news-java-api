247ce4d01fa9353a67645b3e8082882da0d6c3c4
Zeyang Lai <decyctus@gmail.com>
2022-01-16 21:18:27 -0800

    commitMessage

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,10 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 1;
+        double b = 2.2;
+        String c = "cat";
+        System.out.println(a + c);
+    }
+}
